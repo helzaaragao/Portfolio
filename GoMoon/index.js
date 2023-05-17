e94a0cd9d3e76af22826941c49cbd00969b37d29
helzaaragao <helza.batista.aragao@gmail.com>
2023-05-17 16:46:40 -0300

    star

---
+++ GoMoon/index.js
@@ -0,0 +1,8 @@
+console.log("funcionando");
+
+function myFunction() {
+    document.getElementsByClassName("topnav")[0].classList.toggle("responsive");
+    document.getElementsByClassName('mobile-menu')[0].classList.toggle("responsive"); 
+}
+
+
