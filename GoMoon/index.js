8619c96ff45bbd891762c36e74daf96b1737b010
helzaaragao <helza.batista.aragao@gmail.com>
2023-05-26 16:12:52 -0300

    tentando dark mode

--- GoMoon/index.js
+++ GoMoon/index.js
@@ -5,4 +5,15 @@ function myFunction() {
     document.getElementsByClassName('mobile-menu')[0].classList.toggle("responsive"); 
 }
 
+let darkmode = document.querySelector(".darkmode"); 
+
+darkmode.addEventListener('onclick', darkMode());
+
+function darkMode(){ 
+    document.getElementsByClassName("darkmode")[0].classList.toggle("darkact");
+    document.getElementsByName("body")[0].classList.toggle("darkact");
+}
+
+// adicionar o evento onclick com a função que vamos criar 
+
 
