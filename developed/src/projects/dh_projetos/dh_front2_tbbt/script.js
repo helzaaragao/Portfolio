8dc2eb96b01467d5a87ee3d1f0bbe339e290fbb1
helzaaragao <helza.batista.aragao@gmail.com>
2024-01-24 11:17:05 -0300

    verificando como farei com os projetos mais antigos

---
+++ developed/src/projects/dh_projetos/dh_front2_tbbt/script.js
@@ -0,0 +1,89 @@
+const saveButton = document.querySelector('#save-button');
+const cardsContainer = document.querySelector('#card-container');
+const errorMessage = document.querySelector('#error-message');
+const nameInput = document.querySelector('#name');
+const imageInput = document.querySelector('#image');
+const descriptionInput = document.querySelector('#description');
+const memoryInput = document.querySelector('#memory');
+
+const savedCharacters = JSON.parse(localStorage.getItem('character'));
+if (savedCharacters !== null) {
+  savedCharacters.forEach(character => {
+    createCard(character);
+  });
+}
+
+function createCard(character) {
+  const div = document.createElement('div');
+  div.classList.add('w-56', 'bg-[#265F7D]', 'p-2', 'rounded-xl', 'h-80');
+
+  const cardImage = document.createElement('img');
+  cardImage.src = character.urlImage;
+  cardImage.alt = character.name;
+  cardImage.classList.add('w-[208px]', 'h-[140px]', 'rounded-[10px]', 'object-cover');
+  div.appendChild(cardImage);
+
+  const cardTitle = document.createElement('h3');
+  cardTitle.classList.add('font-bold', 'text-center', 'mt-2');
+  cardTitle.innerText = character.name;
+  div.appendChild(cardTitle);
+
+  const cardDescription = document.createElement('p');
+  cardDescription.classList.add('text-xs', 'text-left', 'px-1', 'mt-1');
+  cardDescription.innerText = character.description;
+  div.appendChild(cardDescription);
+
+  cardsContainer.appendChild(div);
+}
+
+function resetModal() {
+    errorMessage.innerText = ''
+    nameInput.value = ''
+    imageInput.value = ''
+    descriptionInput.value = ''
+}
+
+function verification() {
+  const verifyLength =
+    nameInput.value.length > 3 &&
+    descriptionInput.value.length > 3;
+
+  const isEmpty = 
+    nameInput.value.trim().length > 0 &&
+    imageInput.value.trim().length > 0 &&
+    descriptionInput.value.trim().length > 0;
+
+  if (verifyLength && isEmpty) {
+    saveButton.removeAttribute('disabled');
+  } else {
+    saveButton.setAttribute('disabled', true);
+  }
+}
+
+function saveCharacter() {
+  const character = {
+    name: nameInput.value,
+    urlImage: imageInput.value,
+    description: descriptionInput.value,
+  };
+
+  createCard(character);
+
+  if (memoryInput.checked) {
+      if (savedCharacters !== null) {
+        savedCharacters.push(character)
+        localStorage.setItem('character', JSON.stringify(savedCharacters));
+      } else {
+        localStorage.setItem('character', JSON.stringify([character]));
+      }     
+  }
+  resetModal();
+  saveButton.setAttribute('disabled', true); 
+}
+
+nameInput.addEventListener('input', verification);
+imageInput.addEventListener('input', verification);
+descriptionInput.addEventListener('input', verification);
+saveButton.addEventListener('click', saveCharacter);
+
+export default Script;
---
+++ developed/src/routes/index.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+export function RoutesApp(){
+    return(
+        <BrowserRouter>
+        <Routes>      
+    
+                                         
+
+
+        </Routes>
+        </BrowserRouter>
+    )
+}
