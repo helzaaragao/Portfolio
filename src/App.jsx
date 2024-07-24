5d2cc9599ac82944b99831d57aea52228cd62352
helzaaragao <helza.batista.aragao@gmail.com>
2024-07-24 17:53:33 -0300

    API externa + cores principais

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,7 @@
-
 function App() {
   return (
     <div>
-      <a  href="https://afrataiza.github.io/The_big_bang_theory/">Ola</a>
-      <a href="https://github.com/afrataiza/The_big_bang_theory">Repositorio</a>
-            {/* funcionando  */}
+         
     </div>
   )
 }
---
+++ src/styles/themes/default.ts
@@ -0,0 +1,11 @@
+export const defaultTheme = { 
+        'white-smoke': '#F3F3F3', 
+        'timberwolf': '#D9D9D9', 
+        'golden-gate-bridge': '#F44708', 
+        'selective-yellow': '#FCBA04', 
+        'golden-brown': '#9A6D3B', 
+        'turkey-red': '#A50104', 
+        'tyrian-purple': '#550527', 
+        'hunter-green': '#33673B', 
+        'black-brean': '#225500'
+}
